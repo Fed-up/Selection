5a3140868aa7196c99e0c05df2d4357ac73ad19b
Fed-up <tomcurphey12@gmail.com>
2015-06-09 00:02:17 +1000

    Mostly updated
    Need to do final touches

--- app/views/tmpl/public.blade.php
+++ app/views/tmpl/public.blade.php
@@ -7,14 +7,14 @@
 <html>
 	<head>
     	<title>{{ $title or "Selection Cafe" }}</title>  
-      <link rel="shortcut icon" type="image/png" href="images/selection/minitab.png">
+      <link rel="shortcut icon" type="image/png" href="images/selection/minitab1.png">
       <meta name="viewport" content="width=device-width, initial-scale=1.0" > 
       @yield('_header')
       <!-- Dev site css --> 
-      <link rel="stylesheet" type="text/css" href="../sass/compiled_css/dev_selection.css"> 
+      <!-- <link rel="stylesheet" type="text/css" href="../sass/compiled_css/dev_selection.css">  -->
       
       <!-- Live site css  --> 
-      <!-- <link rel="stylesheet" type="text/css" href="../deploy_css/selection.min.css">  -->
+      <link rel="stylesheet" type="text/css" href="../deploy_css/selection.min.css"> 
 
 
       <link rel="stylesheet" type="text/css" href="/packages/jquery-1.11.1.min/vendor/jquery-ui-1.10.4.custom/css/no-theme/jquery-ui-1.10.4.custom.min.css"/>
